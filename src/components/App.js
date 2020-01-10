d3611f16aeb924b611691b7c3ff1a47b3f187eaa
Joshua Grossman <Josh1794@gmail.com>
2020-01-10 15:08:12 -0500

    basic file structure

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Nav from "./Nav";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <header className="App-header">
+        <header className="title">Joshua Grossman</header>
+        <p>Welcome to my website</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/App.css";
+
+function Nav() {
+  return (
+    <div className="App">
+      <nav className="navBar">
+        <div>Portfolio</div>
+        <div>Resume</div>
+        <div>Contact Me</div>
+        <div>About me</div>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
