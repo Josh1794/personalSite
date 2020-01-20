01735b8a9f81559114697229d1d1cbe6202b4bb4
Joshua Grossman <Josh1794@gmail.com>
2020-01-20 16:44:56 -0500

    style test

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,35 +1,85 @@
 import React from "react";
-import MobileNav from "./mobileNav";
-import NormalNav from "./normalNav";
+import { Navbar, Nav, Modal, Button } from "react-bootstrap";
 import "../styles/Nav.css";
 
-export default class Navigation extends React.Component {
+export default class NormalNav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.scrollWin = this.scrollWin.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
   state = {
-    width: window.innerWidth
+    show: false
   };
-
-  componentDidMount() {
-    window.addEventListener("resize", this.handleWindowSizeChange);
+  scrollWin(e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 1000,
+      behavior: "smooth"
+    });
   }
 
-  // make sure to remove the listener
-  // when the component is not mounted anymore
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.handleWindowSizeChange);
+  handleClose() {
+    this.setState({ show: false });
   }
-
-  handleWindowSizeChange = () => {
-    this.setState({ width: window.innerWidth });
-  };
+  handleShow() {
+    this.setState({ show: true });
+  }
 
   render() {
-    const { width } = this.state;
-    const isMobile = width <= 500;
+    return (
+      <div>
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          fixed="top"
+          bg="dark"
+          variant="dark"
+        >
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Navbar.Brand href="/" className="NavBrand">
+              Joshua Grossman
+            </Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="Portfolio">Portfolio</Nav.Link>
+              <Nav.Link onClick={this.handleShow}>Resume</Nav.Link>
+              <Nav.Link href="Contact">Contact Me</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
-    if (isMobile) {
-      return <MobileNav />;
-    } else {
-      return <NormalNav />;
-    }
+        <Modal
+          autoFocus={true}
+          show={this.state.show}
+          onHide={this.handleClose}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Joshua Grossman's Resume</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div>
+              <iframe
+                src="https://drive.google.com/file/d/1YtYsYM6FEHskBKUzoZZK8_qgTt1q-NzJ/preview"
+                width="475px"
+                height="600px"
+                title="My Resume"
+              ></iframe>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <a href="Joshua Grossman's Resume.pdf" download>
+              <Button variant="success">
+                <i class="fas fa-file-download"></i> Download
+              </Button>
+            </a>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
   }
 }
